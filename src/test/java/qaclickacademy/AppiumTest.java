5c644f5e8161b167de245eb2594d5aa284caabf1
manoj <manojkharde@gmail.com>
2022-05-31 15:54:02 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	//This program is called with Jenkins
+	@Test
+	public void NativeAppAndriod()
+	{
+		System.out.println("NativeAppAndriod");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	//This program is called with Jenkins
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+}
